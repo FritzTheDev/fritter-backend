d520be89a0a5743963a49f752ba5c607fc2b4676
Frederick Johnson <fritzj96@gmail.com>
2020-03-31 21:44:32 -0700

    formatted: dbConfig

--- src/core/dbConfig.ts
+++ src/core/dbConfig.ts
@@ -1,16 +1,16 @@
-import { ConnectionOptions } from 'typeorm';
+import { ConnectionOptions } from "typeorm";
 /*
-* bBConfig takes environment variables loaded by DotEnv and plugs them
-* into a config object for createConnection() to use.
-* It also sets the DB type & tells typeorm where to find entities.
-*/
+ * bBConfig takes environment variables loaded by DotEnv and plugs them
+ * into a config object for createConnection() to use.
+ * It also sets the DB type & tells typeorm where to find entities.
+ */
 export const dbConfig: ConnectionOptions = {
-  type: 'postgres',
+  type: "postgres",
   host: process.env.POSTGRES_HOST,
   port: Number(process.env.POSTGRES_PORT),
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
-  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-  synchronize: process.env.POSTGRES_SYNC === '1',
+  entities: [__dirname + "/../**/*.entity{.ts,.js}"],
+  synchronize: process.env.POSTGRES_SYNC === "1"
 };
