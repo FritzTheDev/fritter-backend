cf227a11f761ee6ad4879abadf3becd6255570ee
Frederick Johnson <fritzj96@gmail.com>
2020-03-31 21:33:47 -0700

    added: comments to app.ts

--- src/core/App.ts
+++ src/core/App.ts
@@ -1,20 +1,29 @@
 import { default as express, json as jsonParser } from "express";
 
+// This "App" class sets us the express middleware and routes,
+// and also includes a "listen" function to start the server.
 export class App {
+  // declaring the "app" property type
   app: express.Application;
 
   constructor() {
+    // works off the express default property to keep typescript happy.
     this.app = express();
+
+    // takes all the non-error-handling middleware and sets it up.
+    // TODO: Explore whether error middleware needs to be separate.
     this.initializeMiddleware();
   }
 
   listen() {
+    // starts the app listening and logs that + the port to STDOUT
     this.app.listen(process.env.PORT, () => {
-      console.log(`Running on ${process.env.PORT}`);
+      console.info(`Running on ${process.env.PORT}`);
     });
   }
 
   initializeMiddleware() {
+    // Using the express.json middleware here because why bother with bodyparser?
     this.app.use(jsonParser());
   }
 }
