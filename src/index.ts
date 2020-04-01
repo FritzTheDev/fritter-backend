8aa67b448c03dd309d55c4ada307e14087b495c1
Frederick Johnson <fritzj96@gmail.com>
2020-03-31 21:26:09 -0700

    added: comments to index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,17 @@ import { createConnection } from "typeorm";
 
 import { App, dbConfig } from "./core";
 
+// just a convenient function to run the startup login in.
 const main = async () => {
   // if this throws then the app is worthless anyway
   await createConnection(dbConfig);
 
+  // Creates a new app.
   const app = new App();
+
+  // starts said app listening.
   app.listen();
 }
 
+// actually calls the "startup" function.
 main();
