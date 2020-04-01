4b0b342e82f2aa3d504a8eec3edc995047a929a0
Frederick Johnson <fritzj96@gmail.com>
2020-03-31 21:18:12 -0700

    added: index.ts main function

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,18 @@
 import "reflect-metadata";
 import "dotenv/config";
+import { createConnection } from "typeorm";
 
 import { App } from "./core/App";
+
+const main = async () => {
+  try {
+    await createConnection(dbConfig);
+  } catch (error) {
+    console.log(`Error Connecting To Database: ${error}`);
+  }
+
+  const app = new App();
+  app.listen();
+}
+
+main();
